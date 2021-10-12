5725ca7de5dae246f440151cbd076400dc7e1419
Luis Carlos <luiscarlosnors@gmail.com>
2021-10-11 22:00:54 -0300

    [features/API] Tratando retorno do CEP.

--- app/Http/Controllers/Diarista/ObtemDiaristasPorCEP.php
+++ app/Http/Controllers/Diarista/ObtemDiaristasPorCEP.php
@@ -6,6 +6,7 @@ use App\Http\Controllers\Controller;
 use App\Http\Resources\DiaristaPublico;
 use App\Http\Resources\DiaristaPublicoCollection;
 use App\Models\User;
+use App\Services\ConsultaCEP\ViaCEP;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Http;
 
@@ -17,13 +18,13 @@ class ObtemDiaristasPorCEP extends Controller
      * @param \Illuminate\Http\Request $request
      * @return \Illuminate\Http\Response
      */
-    public function __invoke(Request $request)
+    public function __invoke(Request $request,  ViaCEP $servicoCEP)
     {
-        $cep = $request->cep;
+        $dados = $servicoCEP->buscar($request->cep);
 
-        $resposta = Http::get("https://viacep.com.br/ws/$cep/json/");
-
-        $dados = $resposta->json();
+        if ($dados === false) {
+            return response()->json(['erro' => 'CEP Inválido'], 400);
+        }
 
         return new DiaristaPublicoCollection(
             User::diaristaDisponivelCidade($dados['ibge']),
---
+++ app/Services/ConsultaCEP/ViaCEP.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace App\Services\ConsultaCEP;
+
+use Illuminate\Support\Facades\Http;
+
+class ViaCEP
+{
+    /**
+     * Buscar endereço utilizando a api do viaCEP
+     *
+     * @param string $cep
+     * @return void
+     */
+    public function buscar(string $cep)
+    {
+        $resposta = Http::get("https://viacep.com.br/ws/$cep/json/");
+
+        if ($resposta->failed()) {
+            return false;
+        }
+
+        $dados = $resposta->json();
+
+        if (isset($dados['erro']) && $dados['erro'] === true) {
+            return false;
+        }
+
+        return $dados;
+    }
+}

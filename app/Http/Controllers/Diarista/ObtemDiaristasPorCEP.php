12d6ab8da6fbfdfdc13e0ddbe8e910cebe89de3e
Luis Carlos <luiscarlosnors@gmail.com>
2021-10-12 22:16:55 -0300

    [features/API] Criado interface para desacoplar serviço.

--- app/Http/Controllers/Diarista/ObtemDiaristasPorCEP.php
+++ app/Http/Controllers/Diarista/ObtemDiaristasPorCEP.php
@@ -6,9 +6,7 @@ use App\Http\Controllers\Controller;
 use App\Http\Resources\DiaristaPublico;
 use App\Http\Resources\DiaristaPublicoCollection;
 use App\Models\User;
-use App\Services\ConsultaCEP\ViaCEP;
-use Illuminate\Http\Request;
-use Illuminate\Support\Facades\Http;
+use App\Services\ConsultaCEP\ConsultaCEPInterface;use Illuminate\Http\Request;
 
 class ObtemDiaristasPorCEP extends Controller
 {
@@ -18,7 +16,7 @@ class ObtemDiaristasPorCEP extends Controller
      * @param \Illuminate\Http\Request $request
      * @return \Illuminate\Http\Response
      */
-    public function __invoke(Request $request,  ViaCEP $servicoCEP)
+    public function __invoke(Request $request,  ConsultaCEPInterface $servicoCEP)
     {
         $dados = $servicoCEP->buscar($request->cep);
 

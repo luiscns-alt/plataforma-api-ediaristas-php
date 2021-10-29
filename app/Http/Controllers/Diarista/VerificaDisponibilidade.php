044ee985e20c17dc0330074390524d5a0b328b78
Luis Carlos <luiscarlosnors@gmail.com>
2021-10-28 22:23:23 -0300

    [Feature] Verificação de disponibilidade de diaristas para o CEP.

---
+++ app/Http/Controllers/Diarista/VerificaDisponibilidade.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace App\Http\Controllers\Diarista;
+
+use App\Actions\Diarista\ObterDiaristasPorCEP;
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+
+class VerificaDisponibilidade extends Controller
+{
+    /**
+     * Handle the incoming request.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function __invoke(Request $request, ObterDiaristasPorCEP $action)
+    {
+        $request->validate([
+            'cep' => ['required', 'numeric']
+        ]);
+
+        [$diaristasCollection] = $action->executar($request->cep);
+
+        return resposta_padrao(
+            "Disponibilidade verificada com sucesso",
+            "sucesso",
+            200,
+            ["disponibilidade" => $diaristasCollection->isNotEmpty()]
+        );
+    }
+}
--- routes/api.php
+++ routes/api.php
@@ -2,5 +2,7 @@
 
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\Diarista\ObtemDiaristasPorCEP;
+use App\Http\Controllers\Diarista\VerificaDisponibilidade;
 
 Route::get('/diaristas/localidades', ObtemDiaristasPorCEP::class)->name('diaristas.busca_por_cep');
+Route::get('/diaristas/disponibilidade', VerificaDisponibilidade::class)->name('enderecos.disponibilidade');

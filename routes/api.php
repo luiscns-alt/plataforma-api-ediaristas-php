7de50627f7997b4a6b08ecf6354737b949d8e9ce
Luis Carlos <luiscarlosnors@gmail.com>
2021-10-29 23:58:09 -0300

    [Feature] Listando os serviçõs cadastrados.

--- routes/api.php
+++ routes/api.php
@@ -4,7 +4,9 @@ use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\Diarista\ObtemDiaristasPorCEP;
 use App\Http\Controllers\Diarista\VerificaDisponibilidade;
 use App\Http\Controllers\Endereco\BuscaCepApiExterna;
+use App\Http\Controllers\Servico\ObtemServicos;
 
 Route::get('/diaristas/localidades', ObtemDiaristasPorCEP::class)->name('diaristas.busca_por_cep');
 Route::get('/diaristas/disponibilidade', VerificaDisponibilidade::class)->name('enderecos.disponibilidade');
 Route::get('/enderecos', BuscaCepApiExterna::class)->name('enderecos.cep');
+Route::get('/servicos', ObtemServicos::class)->name('servicos.index');

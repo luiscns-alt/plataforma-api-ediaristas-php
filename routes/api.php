409bb487b390a31e2f645da13de1b7109411bb45
Luis Carlos <luiscarlosnors@gmail.com>
2021-11-11 16:37:05 -0300

    [Feature] Realizando o login

--- routes/api.php
+++ routes/api.php
@@ -6,9 +6,13 @@ use App\Http\Controllers\Diarista\VerificaDisponibilidade;
 use App\Http\Controllers\Endereco\BuscaCepApiExterna;
 use App\Http\Controllers\IndexController;
 use App\Http\Controllers\Servico\ObtemServicos;
+use App\Http\Controllers\Usuario\AutenticacaoController;
 use App\Http\Controllers\Usuario\CadastroController;
 
 Route::get('/', IndexController::class);
+
+Route::post('/token', [AutenticacaoController::class, 'login'])->name('autenticacao.login');
+Route::get('/me', [AutenticacaoController::class, 'me'])->name('usuario.show');
 
 Route::get('/diaristas/localidades', ObtemDiaristasPorCEP::class)->name('diaristas.busca_por_cep');
 Route::get('/diaristas/disponibilidade', VerificaDisponibilidade::class)->name('enderecos.disponibilidade');

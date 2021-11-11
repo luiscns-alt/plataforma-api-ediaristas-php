50d009b53523e218f37c6f883e036d3e91554f6d
Luis Carlos <luiscarlosnors@gmail.com>
2021-11-10 22:52:05 -0300

    [Feature] Desenvolvido Controller e Action cadastro de usuários.

--- routes/api.php
+++ routes/api.php
@@ -6,10 +6,12 @@ use App\Http\Controllers\Diarista\VerificaDisponibilidade;
 use App\Http\Controllers\Endereco\BuscaCepApiExterna;
 use App\Http\Controllers\IndexController;
 use App\Http\Controllers\Servico\ObtemServicos;
+use App\Http\Controllers\Usuario\CadastroController;
 
-Route::get('/',IndexController::class);
+Route::get('/', IndexController::class);
 
 Route::get('/diaristas/localidades', ObtemDiaristasPorCEP::class)->name('diaristas.busca_por_cep');
 Route::get('/diaristas/disponibilidade', VerificaDisponibilidade::class)->name('enderecos.disponibilidade');
 Route::get('/enderecos', BuscaCepApiExterna::class)->name('enderecos.cep');
 Route::get('/servicos', ObtemServicos::class)->name('servicos.index');
+Route::post('/usuarios', [CadastroController::class, 'store'])->name('usuarios.create');

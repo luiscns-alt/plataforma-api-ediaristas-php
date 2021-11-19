2ba526b2247a0df2c5a331c637fd4fc2d3e8b6a9
Luis Carlos <luiscarlosnors@gmail.com>
2021-11-18 22:58:08 -0300

    [Feature] Logando usuário na criação.

--- app/Http/Resources/Usuario.php
+++ app/Http/Resources/Usuario.php
@@ -3,9 +3,16 @@
 namespace App\Http\Resources;
 
 use Illuminate\Http\Resources\Json\JsonResource;
+use Illuminate\Support\Facades\Auth;
 
 class Usuario extends JsonResource
 {
+
+    public function __construct($resource, private string $token = '')
+    {
+        parent::__construct($resource, $token);
+    }
+
     /**
      * Transform the resource into an array.
      *
@@ -14,7 +21,7 @@ class Usuario extends JsonResource
      */
     public function toArray($request): array
     {
-        return [
+        $formato = [
             "id" => $this->id,
             "nome_completo" => $this->nome_completo,
             "cpf" => $this->cpf,
@@ -26,5 +33,17 @@ class Usuario extends JsonResource
             "foto_usuario" => $this->foto_usuario,
             "chave_pix" => null,
         ];
+
+        if ($this->token !== '') {
+            return $formato + [
+                "token" => [
+                    "access" => $this->token,
+                    "refresh" => $this->token,
+                    "token_type" => "bearer",
+                    "expires_in" => Auth::factory()->getTTL() * 60,
+                ]
+            ];
+        }
+        return $formato;
     }
 }

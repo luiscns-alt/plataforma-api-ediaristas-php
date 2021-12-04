c071beb5385daedf8df6ddc7402e83d077700aa7
Luis Carlos <luiscarlosnors@gmail.com>
2021-12-03 23:11:34 -0300

    Fear: Validando o preço de atendimento da diária.

--- app/Http/Requests/DiariaRequest.php
+++ app/Http/Requests/DiariaRequest.php
@@ -4,6 +4,7 @@ namespace App\Http\Requests;
 
 use App\Rules\HoraFinalDiaria;
 use App\Rules\HoraInicioDiaria;
+use App\Rules\PrecoDiaria;
 use App\Rules\TempoAtendimentoDiaria;
 use Illuminate\Foundation\Http\FormRequest;
 
@@ -38,7 +39,7 @@ class DiariaRequest extends FormRequest
                 new HoraFinalDiaria($this),
                 new TempoAtendimentoDiaria($this)
             ],
-            "preco" => ['required'],
+            "preco" => ['required', new PrecoDiaria($this)],
 
             "logradouro" => ['required'],
             "numero" => ['required'],

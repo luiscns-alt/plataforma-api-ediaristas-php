629a23a53fff81775d73e2948a515fef1d6683ae
Luis Carlos <luiscarlosnors@gmail.com>
2021-12-21 21:14:47 -0300

    Feature: Detalhe do serviço e cliente do resource de diária.

--- app/Models/Diaria.php
+++ app/Models/Diaria.php
@@ -4,6 +4,7 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
 
 class Diaria extends Model
 {
@@ -13,4 +14,24 @@ class Diaria extends Model
      * Campos bloqueados na definição de dados em massa.
      */
     protected $guarded = ['motivo_cancelamento', 'created_at', 'updated_at'];
+
+    /**
+     * Define a relação de servico
+     *
+     * @return BelongsTo
+     */
+    public function servico(): BelongsTo
+    {
+        return $this->belongsTo(Servico::class);
+    }
+
+    /**
+     * Define a relação de cliente
+     *
+     * @return BelongsTo
+     */
+    public function cliente(): BelongsTo
+    {
+        return $this->belongsTo(User::class, 'cliente_id');
+    }
 }

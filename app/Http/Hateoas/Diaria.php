6524fc0aaeca6688f5f16c6d8ed20eba9d231736
Luis Carlos <luiscarlosnors@gmail.com>
2022-01-09 00:10:51 -0300

    Ajustando detalhes no código.

--- app/Http/Hateoas/Diaria.php
+++ app/Http/Hateoas/Diaria.php
@@ -7,6 +7,12 @@ use Illuminate\Database\Eloquent\Model;
 class Diaria extends HateoasBase implements HateoasInterface
 {
 
+    /**
+     * Retorna os links do Hateoas para a diária
+     *
+     * @param Model|null $diaria
+     * @return array
+     */
     public function links(?Model $diaria): array
     {
         if ($diaria->status == 1) {
